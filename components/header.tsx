3ef8e3f891b9b3fcd4b6867b2f514fd799fce65d
Daniel12346 <danezoki2@gmail.com>
2025-05-08 21:42:07 +0200

    refactor: remove unused boilerplate components

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import AuthButton from "./header-auth";
+
+export default function Header() {
+  return (
+    <div className="flex flex-col gap-16 items-center">
+      <AuthButton />
+    </div>
+  );
+}
