c3b19a7712036cd92e657152f92b61c1a68a8633
Daniel12346 <danezoki2@gmail.com>
2025-05-09 21:42:21 +0200

    feat: update Header component layout and include ThemeSwitcher

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,11 @@
 import AuthButton from "./header-auth";
+import { ThemeSwitcher } from "./theme-switcher";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
+    <div className="flex w-full gap-16 items-center justify-between px-4 py-2 bg-background border-b border-border">
       <AuthButton />
+      <ThemeSwitcher />
     </div>
   );
 }
