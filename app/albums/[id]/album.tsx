7c3bde95ef7813533c13b512e834107ad6e7766c
Daniel12346 <danezoki2@gmail.com>
2025-05-14 19:35:15 +0200

    fix: update album component to fetch album with tracks and artist details, enhance track rendering, and improve queue functionality

--- app/albums/[id]/album.tsx
+++ app/albums/[id]/album.tsx
@@ -1,8 +1,9 @@
 "use client";
 import LikeAlbum from "@/components/likeAlbum";
-import { getAlbumWithTracks, getMyAuthUserData } from "@/lib/database";
+import { getAlbumWithTracksAndArtist, getAuthUser } from "@/lib/database";
 import { useStore } from "@/state/store";
 import { createClient } from "@/utils/supabase/client";
+import { ListEndIcon, ListStartIcon } from "lucide-react";
 import useSWR from "swr";
 
 export default function Album({ id }: { id: string }) {
@@ -11,8 +12,11 @@ export default function Album({ id }: { id: string }) {
     data: albumWithTracks,
     isLoading,
     error,
-  } = useSWR(["albumWithTracks", id], () => getAlbumWithTracks(supabase, id));
-  useSWR("me", () => getMyAuthUserData(supabase));
+  } = useSWR(["getAlbumWithTracksAndArtist", id], () =>
+    getAlbumWithTracksAndArtist(supabase, id)
+  );
+  const addTrackToQueue = useStore((state) => state.addToQueue);
+  useSWR("getAuthUser", () => getAuthUser(supabase));
   const setTrackUrl = useStore((state) => state.setTrackUrl);
   if (error) {
     return <div>Error loading album</div>;
@@ -32,19 +36,50 @@ export default function Album({ id }: { id: string }) {
       </div>
 
       {albumWithTracks.cover_url && (
-        <img src={albumWithTracks.cover_url} alt={albumWithTracks.title} />
+        <img
+          src={albumWithTracks.cover_url}
+          alt={albumWithTracks.title}
+          width={50}
+        />
       )}
       <h3>Tracks</h3>
       <ul>
         {albumWithTracks.tracks.map((track) => (
           <li
-            className="bg-red-300"
+            className="bg-red-300 flex items-center justify-between p-2"
             key={track.id}
             onClick={() => {
               setTrackUrl(track.url);
             }}
           >
-            <span>{track.title}</span>
+            <div>
+              <span className="text-lg">{track.title}</span>
+              {/* TODO: artist names (not necessarily the same as album artist names - featured artists etc.)*/}
+            </div>
+            <div className="flex items-center gap-2">
+              {/* TODO: correct type for length */}
+              <span className="text-md font-light">
+                {track.length as string}
+              </span>
+              <ListStartIcon size={16} />
+              <ListEndIcon
+                size={16}
+                onClick={() =>
+                  addTrackToQueue({
+                    ...track,
+                    albumId: albumWithTracks.id,
+                    albumCoverUrl: albumWithTracks.cover_url || "",
+                    albumName: albumWithTracks.title,
+                    artists: albumWithTracks.artists.map((artist) => ({
+                      name: artist.name,
+                      id: artist.id,
+                    })),
+                  })
+                }
+              />
+              {/* TODO: replace with LikeTrack */}
+              <LikeAlbum size={16} albumID={id} />
+            </div>
           </li>
         ))}
       </ul>
