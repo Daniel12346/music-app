052be62ecd9f0ae77dbac02d7fd6a968d1ef73e0
Daniel12346 <danezoki2@gmail.com>
2025-05-14 22:22:28 +0200

    fix: remove Hero component from Home page for cleaner layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Hero from "@/components/header";
-
 export default async function Home() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">Music app</main>
     </>
   );
