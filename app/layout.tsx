aa47baafdb585e68419078f32afe1d1536b610a9
Daniel12346 <danezoki2@gmail.com>
2025-05-14 19:34:03 +0200

    fix: restructure layout to include Queue component and improve child rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
 import Player from "@/components/audio-player";
+import Queue from "@/components/queue";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -28,8 +29,13 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           <Providers>
-            {children}
-            <Player />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 w-full">
+              <div className="md:col-start-2 bg-green-200 ">{children}</div>
+              <Player />
+              <div className="hidden md:block md:col-start-3 bg-blue-200/20  px-5">
+                <Queue />
+              </div>
+            </div>
           </Providers>
         </main>
       </body>
