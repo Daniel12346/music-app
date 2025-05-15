9e6836b3bf6938a7138d870afb5bd487cec565c1
Daniel12346 <danezoki2@gmail.com>
2025-05-15 22:39:50 +0200

    feat: refactor layout to use ResizablePanelGroup for improved UI structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
-import Player from "@/components/audio-player";
+import AudioPlayer from "@/components/audio-player";
 import Queue from "@/components/queue";
 import Header from "@/components/header";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -27,19 +32,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+      <body className="bg-background text-foreground min-h-screen">
+        <main className=" flex flex-col items-center">
           <Providers>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 w-full">
-              <div className="col-span-full">
-                <Header />
-              </div>
-              <div className="md:col-start-2 bg-green-200 ">{children}</div>
-              <Player />
-              <div className="hidden md:block md:col-start-3 bg-blue-200/20  px-5">
-                <Queue />
-              </div>
+            <div className="w-full">
+              <Header />
             </div>
+            <ResizablePanelGroup
+              direction="horizontal"
+              className="min-h-screen"
+            >
+              <ResizablePanel className="hidden md:block" defaultSize={25}>
+                //TODO: this panel
+              </ResizablePanel>
+              <ResizableHandle withHandle className="hidden md:block" />
+              <ResizablePanel defaultSize={50}>{children}</ResizablePanel>
+              <ResizableHandle withHandle className="hidden md:block" />
+              <ResizablePanel className="hidden md:block" defaultSize={25}>
+                <span>Queue</span>
+                <Queue />
+              </ResizablePanel>
+            </ResizablePanelGroup>
           </Providers>
         </main>
       </body>
