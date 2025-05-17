122fd8ce656a9c6ac60cffa4e8ec6bae301c8cef
Daniel12346 <danezoki2@gmail.com>
2025-05-17 21:41:49 +0200

    refactor: simplified structure, styling

--- components/queue.tsx
+++ components/queue.tsx
@@ -6,26 +6,23 @@ export default function Queue() {
   const { queue, removeFromQueue } = useStore();
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex flex-col gap-2">
-        {queue.map((track) => (
-          <div key={track.id} className="flex items-center gap-2">
-            {/* //TODO: track cover art (album art)	 */}
-            <img
-              src={track.albumCoverUrl}
-              alt={track.title}
-              className="w-16 h-16 rounded"
-            />
-            <div>
-              <h2 className="text-lg font-bold">{track.title}</h2>
-              {track.artists.map((artist) => (
-                <Link href={"/artists/" + artist.id}>
-                  <span>{artist.name}</span>
-                </Link>
-              ))}
-            </div>
+      {queue.map((track) => (
+        <div key={track.id} className="flex items-center gap-2">
+          <img
+            src={track.albumCoverUrl}
+            alt={track.title}
+            className="w-16 h-16 rounded"
+          />
+          <div>
+            <h2 className="text-lg font-bold">{track.title}</h2>
+            {track.artists.map((artist) => (
+              <Link href={"/artists/" + artist.id} key={artist.id}>
+                <span className="font-light">{artist.name}</span>
+              </Link>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 }
