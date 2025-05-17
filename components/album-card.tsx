694f1215952b672483bd06d1b8134bcf34f28edf
Daniel12346 <danezoki2@gmail.com>
2025-05-17 21:31:37 +0200

    feat: added optional size prop to album-card

--- components/album-card.tsx
+++ components/album-card.tsx
@@ -1,14 +1,22 @@
 import { Tables } from "@/database.types";
 import Link from "next/link";
-import LikeAlbum from "./likeAlbum";
+import LikeAlbum from "./like-album";
+import { cn } from "@/lib/utils";
 
 type ArtistInfo = Pick<Tables<"artists">, "name" | "id">;
 type Props = Pick<Tables<"albums">, "id" | "title" | "cover_url"> & {
   artists: ArtistInfo[];
+  size?: "large";
 };
-export default function AlbumCard({ id, title, cover_url, artists }: Props) {
+export default function AlbumCard({
+  id,
+  title,
+  cover_url,
+  artists,
+  size,
+}: Props) {
   return (
-    <div className="w-[120px] group">
+    <div className={cn("w-[120px] group", size === "large" && "w-[300px]")}>
       <div className="truncate text-lg flex-col">
         <div className="-mb-2 font-extralight text-muted-foreground">
           {/* TODO: separator */}
@@ -27,16 +35,13 @@ export default function AlbumCard({ id, title, cover_url, artists }: Props) {
           src={cover_url || ""}
           alt={title || ""}
           width={120}
-          className="rounded-md"
+          height={120}
+          className="rounded-md w-full"
         />
         <div className="hidden group-hover:block absolute top-0 right-0">
-          <LikeAlbum albumID={id} size={20} />
+          <LikeAlbum albumID={id} size={size === "large" ? 32 : 16} />
         </div>
       </div>
     </div>
   );
-
-  //   <div className="self-end">
-  //     <LikeAlbum albumID={album.id} />
-  //   </div>;
 }
