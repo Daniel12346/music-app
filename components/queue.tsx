e7c95a2baf2f5319d62348578f9e37bf794237fa
Daniel12346 <danezoki2@gmail.com>
2025-05-18 21:12:31 +0200

    refactor: enhance queue component structure, add index tracking for current track

--- components/queue.tsx
+++ components/queue.tsx
@@ -1,25 +1,45 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { useStore } from "@/state/store";
+import { XIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Queue() {
-  const { queue, removeFromQueue } = useStore();
+  const { queue, removeFromQueue, idxOfCurrentTrackInQueue } = useStore();
   return (
     <div className="flex flex-col gap-2">
-      {queue.map((track) => (
-        <div key={track.id} className="flex items-center gap-2">
-          <img
-            src={track.albumCoverUrl}
-            alt={track.title}
-            className="w-16 h-16 rounded"
-          />
-          <div>
+      {queue.map((track, idx) => (
+        //track.id alone can't be used as key because the same track can be added to queue multiple times
+        //TOOD: find better key
+        <div
+          key={track.id + idx}
+          className={cn(
+            "flex items-center gap-2",
+            idx === idxOfCurrentTrackInQueue && "bg-green-500/40"
+          )}
+        >
+          <Link href={"/albums/" + track.albumId}>
+            <img
+              src={track.albumCoverUrl}
+              alt={track.title}
+              className="w-16 h-16 rounded"
+            />
+          </Link>
+          <div className="flex-1">
             <h2 className="text-lg font-bold">{track.title}</h2>
             {track.artists.map((artist) => (
               <Link href={"/artists/" + artist.id} key={artist.id}>
                 <span className="font-light">{artist.name}</span>
               </Link>
             ))}
+          </div>
+          <div>
+            <XIcon
+              className="cursor-pointer"
+              onClick={() => {
+                removeFromQueue(track.id, idx);
+              }}
+            />
           </div>
         </div>
       ))}
