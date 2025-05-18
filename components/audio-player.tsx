3b9a823e4ac7310e615701a2ffe136f59543ebf6
Daniel12346 <danezoki2@gmail.com>
2025-05-18 21:14:15 +0200

    added functions for playing next and previous tracks

--- components/audio-player.tsx
+++ components/audio-player.tsx
@@ -1,27 +1,46 @@
 "use client";
 import { useStore } from "@/state/store";
 import { StopCircleIcon } from "lucide-react";
+import Link from "next/link";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 // import 'react-h5-audio-player/lib/styles.less' Use LESS
 // import 'react-h5-audio-player/src/styles.scss' Use SASS
 
 export default function Player() {
-  const { trackUrl, setTrackUrl } = useStore();
-  if (!trackUrl) {
+  const { currentTrack, playNextTrack, playPrevTrack } = useStore();
+  if (!currentTrack) {
     return null;
   }
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50">
-      <StopCircleIcon onClick={() => setTrackUrl("")}></StopCircleIcon>
       <AudioPlayer
+        style={{ background: "black", opacity: "70%" }}
         showSkipControls
+        showJumpControls={false}
+        onClickNext={() => playNextTrack()}
+        onEnded={() => playNextTrack()}
+        onClickPrevious={() => playPrevTrack()}
         //TODO: track title, artist, album, cover art
         //TODO: custom icons
-        header={<h1 className="text-red bg-red-500 absolute">Audio Player</h1>}
-        src={trackUrl}
-        onPlay={(e) => console.log("onPlay")}
+        header={
+          <div className="flex bg-black w-fit">
+            <img src={currentTrack.albumCoverUrl} width={50} height={50} />
+            <div className="flex flex-col">
+              <span>{currentTrack.title}</span>
+              <span>
+                {currentTrack.artists.map((artist) => (
+                  <Link href={"/artists" + artist.id} key={artist.id}>
+                    <span>{artist.name}</span>
+                  </Link>
+                ))}
+              </span>
+            </div>
+          </div>
+        }
+        src={currentTrack.url}
+        onPlay={() => console.log("onPlay")}
       />
     </div>
   );
