52fa6719a86c2375a78a0b212b2503feef1d1a3f
Daniel12346 <danezoki2@gmail.com>
2025-05-23 22:09:47 +0200

    fix: add margin-top to layout container for improved spacing

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -4,7 +4,7 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl flex flex-col gap-12 items-center">
+    <div className="max-w-7xl mt-12 flex flex-col gap-12 place-items-center">
       {children}
     </div>
   );
