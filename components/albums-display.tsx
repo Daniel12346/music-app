829d00e459c6868d9d734a329601288285f8bd9a
Daniel12346 <danezoki2@gmail.com>
2025-05-23 22:13:14 +0200

    feat: add optional props to Albums component for artist name and release date display

--- components/albums-display.tsx
+++ components/albums-display.tsx
@@ -3,8 +3,14 @@ import AlbumCard from "./album-card";
 
 type Props = {
   albums: AlbumsWithArtists;
+  showArtistName?: boolean;
+  showReleasedAt?: boolean;
 };
-export default function Albums({ albums }: Props) {
+export default function Albums({
+  albums,
+  showArtistName = true,
+  showReleasedAt = false,
+}: Props) {
   return (
     <div className="@container">
       <ul className="grid grid-cols-2 @md:grid-cols-3 @lg:grid-cols-4">
@@ -12,9 +18,11 @@ export default function Albums({ albums }: Props) {
           <li className="flex place-content-center" key={album.id}>
             <div className="">
               <AlbumCard
+                showArtistName={showArtistName}
                 cover_url={album.cover_url}
                 title={album.title}
                 id={album.id}
+                released_at={showReleasedAt ? album.released_at : null}
                 artists={album.artists_albums.map((a) => a.artists)}
               />
             </div>
