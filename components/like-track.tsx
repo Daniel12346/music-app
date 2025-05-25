2b4e129a73504a04189044f32cebc96464387dd2
Daniel12346 <danezoki2@gmail.com>
2025-05-25 22:50:34 +0200

    feat: add LikeTrack component for liking and unliking tracks

---
+++ components/like-track.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { getTracksLikedByUser, likeTrack, unlikeTrack } from "@/lib/database";
+import { cn } from "@/lib/utils";
+import { createClient } from "@/utils/supabase/client";
+import { HeartIcon } from "lucide-react";
+import useSWR from "swr";
+
+export default function LikeTrack({
+  trackID,
+  size = 16,
+  strokeColor,
+}: {
+  trackID: string;
+  size?: number;
+  strokeColor?: string;
+}) {
+  const supabase = createClient();
+  const { data: myData } = useSWR("me", async () => {
+    const { data } = await supabase.auth.getUser();
+    return data;
+  });
+  const myID = myData?.user?.id;
+  const { data: liked, mutate: mutateLiked } = useSWR(
+    myID ? ["getTracksLikedByUser", myID] : null,
+    () => getTracksLikedByUser(supabase, myID)
+  );
+  const isTrackLiked =
+    liked?.some((likedTrack) => likedTrack.id === trackID) ?? false;
+
+  if (!myID) {
+    return null;
+  }
+  return (
+    <HeartIcon
+      className={cn(
+        "cursor-pointer",
+        isTrackLiked && "fill-red-600",
+        strokeColor && `stroke-${strokeColor}`
+      )}
+      size={size}
+      onClick={async () => {
+        if (isTrackLiked) {
+          await unlikeTrack(supabase, myID, trackID);
+          mutateLiked((prev) => prev?.filter((liked) => liked.id !== trackID));
+        } else {
+          await likeTrack(supabase, myID, trackID);
+          mutateLiked();
+        }
+      }}
+    />
+  );
+}
