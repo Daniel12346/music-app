948aa6da3181b18d859d7e9629a1eb94c062afd2
Daniel12346 <danezoki2@gmail.com>
2025-05-26 22:22:59 +0200

    feat: implement Liked component to display user's liked tracks and history

---
+++ components/tracks-sidebar.tsx
@@ -0,0 +1,28 @@
+import { getTracksLikedByUser, getUserHistoryTracks } from "@/lib/database";
+import { createClient } from "@/utils/supabase/server";
+import useSWR, { SWRConfig, unstable_serialize } from "swr";
+import TracksLiked from "./tracks-liked";
+import TracksHistory from "./tracks-history";
+
+export default async function Liked() {
+  const supabase = await createClient();
+  const { data: myData } = await supabase.auth.getUser();
+  const myID = myData.user?.id;
+  const myLikedTracks = await getTracksLikedByUser(supabase, myID);
+  const tracksHistory = await getUserHistoryTracks(supabase, myID);
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          [unstable_serialize(["getTracksLikedByUser", myID])]: myLikedTracks,
+          [unstable_serialize(["getUserHistoryTracks", myID])]: tracksHistory,
+        },
+      }}
+    >
+      <aside className="flex flex-col gap-2 p-2">
+        <TracksLiked />
+        <TracksHistory />
+      </aside>
+    </SWRConfig>
+  );
+}
