439158bd9b54491ddc811c233bfbe506d719ba4a
Daniel12346 <danezoki2@gmail.com>
2025-05-30 23:20:57 +0200

    feat: enhance TrackOptionsButton to display multiple artists in dropdown

--- components/track-options-button.tsx
+++ components/track-options-button.tsx
@@ -6,11 +6,14 @@ import {
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
 import {
+  ChevronRightIcon,
   Disc2Icon,
   Download,
   EllipsisVerticalIcon,
   ListEndIcon,
   ListStartIcon,
+  UserIcon,
+  UsersIcon,
 } from "lucide-react";
 import { TrackWithExtra } from "@/state/store";
 import Link from "next/link";
@@ -28,8 +31,34 @@ export default function TrackOptionsButton({
         <EllipsisVerticalIcon className="cursor-pointer block " />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-48">
-        <DropdownMenuItem className="flex flex-between">
-          {/* <Link href={`/artists/${track.artists.id}`}>View artist</Link> */}
+        <DropdownMenuItem className="flex justify-between">
+          {track.artists.length === 1 ? (
+            <>
+              <UserIcon size={20} />
+              <Link href={`/artists/${track.artists[0].id}`}>View artist</Link>
+            </>
+          ) : (
+            <>
+              <UsersIcon size={20} />
+              <Link href={`/artists/${track.artists[0].id}`} className="flex">
+                View artist{" "}
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <ChevronRightIcon size={20} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    {track.artists.map((artist) => (
+                      <DropdownMenuItem key={artist.id}>
+                        <Link href={`/artists/${artist.id}`}>
+                          {artist.name}
+                        </Link>
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </Link>
+            </>
+          )}
         </DropdownMenuItem>
         <DropdownMenuItem>
           <Link
