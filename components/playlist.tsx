82344a18d97272261149d510ac38b3d284ab0620
Daniel12346 <danezoki2@gmail.com>
2025-06-06 22:08:11 +0200

    feat: add Playlist component to display user playlists with previews

---
+++ components/playlist.tsx
@@ -0,0 +1,53 @@
+import { Database } from "@/database.types";
+import { PlaylistsWithPreview } from "@/lib/database";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+//playlist track is the type returned by getUserPlaylistsWithPreview
+
+interface Props {
+  id: string;
+  name: string;
+  image_url: string | null;
+  playlists_tracks: PlaylistsWithPreview[0]["playlists_tracks"];
+}
+export default function Playlist({
+  id,
+  name,
+  image_url,
+  playlists_tracks,
+}: Props) {
+  return (
+    <Link href={`/playlists/${id}`} key={id}>
+      <span className="font-semibold">{name}</span>
+      <div className="w-24 h-24 bg-muted">
+        {/* //TODO: default image */}
+        {image_url ? (
+          <img src={image_url} width={10} height={10} className="w-full" />
+        ) : (
+          <div className="grid grid-cols-2 grid-rows-2 border-1 border-muted-foreground rounded-md w-full">
+            {playlists_tracks.slice(0, 4).map((playlistTrack, i) => {
+              return (
+                playlistTrack.albums.cover_url && (
+                  <img
+                    key={playlistTrack.track_id}
+                    src={playlistTrack.albums.cover_url}
+                    width={10}
+                    height={10}
+                    className={cn(
+                      "w-full",
+                      i === 0 && "rounded-tl-md",
+                      i === 1 && "rounded-tr-md",
+                      i === 2 && "rounded-bl-md",
+                      i === 3 && "rounded-br-md"
+                    )}
+                  />
+                )
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </Link>
+  );
+}
