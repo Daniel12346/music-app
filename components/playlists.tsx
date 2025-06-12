4cee2698ac2ed1a9f9328efeef2855d18b3f3a49
Daniel12346 <danezoki2@gmail.com>
2025-06-12 12:52:03 +0200

    refactor: improve readability of Link component in Playlists

--- components/playlists.tsx
+++ components/playlists.tsx
@@ -24,7 +24,11 @@ export default function Playlists() {
     <div className="@container">
       <div className="grid grid-cols-2 @md:grid-cols-3 @lg:grid-cols-4">
         {myPlaylists?.map((playlist) => (
-          <Link href={`/playlists/${playlist.id}`} key={playlist.id}>
+          <Link
+            href={`/playlists/${playlist.id}`}
+            key={playlist.id}
+            className="flex justify-center"
+          >
             <PlaylistCard
               {...playlist}
               album_cover_urls={playlist.playlists_tracks.map(
