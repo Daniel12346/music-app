e1549b589e92c9581101771841675f3641c6b198
Daniel12346 <danezoki2@gmail.com>
2025-06-18 00:45:01 +0200

    feat: enhance audio player with LikeTrack component and improve layout

--- components/audio-player.tsx
+++ components/audio-player.tsx
@@ -9,7 +9,8 @@ import { createClient } from "@/utils/supabase/client";
 import { addTrackToHistory, getUserHistoryTracks } from "@/lib/database";
 import { useEffect } from "react";
 import TrackArtists from "./track-artists";
-import { ListIcon } from "lucide-react";
+import { ListIcon, Repeat1Icon, ShuffleIcon } from "lucide-react";
+import LikeTrack from "./like-track";
 export default function Player() {
   const { currentTrack, playNextTrack, playPrevTrack } = useTrackStore();
   const supabase = createClient();
@@ -40,7 +41,7 @@ export default function Player() {
     return null;
   }
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50">
+    <div className="fixed bottom-0 left-0 right-0 z-50 w-full">
       <AudioPlayer
         showSkipControls
         showJumpControls={false}
@@ -51,21 +52,31 @@ export default function Player() {
         //TODO: custom icons
         header={
           <div className="w-full flex items-center justify-between">
-            <div className="flex w-fit gap-2">
+            <div className="flex w-fit gap-2 ">
               <Link
                 href={"/albums/" + currentTrack.albumId}
                 className="cursor-pointer"
               >
                 <img src={currentTrack.albumCoverUrl} width={50} height={50} />
               </Link>
-              <div className="flex flex-col gap-2 justify-center">
-                <span>{currentTrack.title}</span>
-                <span>
+              <div className="flex flex-col gap-2 max-w-30 md:max-w-60 justify-center">
+                <span className="truncate">{currentTrack.title}</span>
+                <span className="truncate">
                   <TrackArtists artists={currentTrack.artists}></TrackArtists>
                 </span>
               </div>
+              <div className="flex items-center">
+                <LikeTrack
+                  size={20}
+                  trackID={currentTrack.id}
+                  trackAlbumID={currentTrack.albumId}
+                />
+              </div>
             </div>
-            <div>
+
+            <div className="flex  gap-3 md:gap-4">
+              <Repeat1Icon />
+              <ShuffleIcon />
               <Link href={`/queue`}>
                 <ListIcon />
               </Link>
@@ -74,6 +85,7 @@ export default function Player() {
         }
         src={currentTrack.url}
         // onPlay={() => console.log(currentTrack)}
+        customAdditionalControls={[]}
       />
     </div>
   );
