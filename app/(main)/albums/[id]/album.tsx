f98a79cc9bb5121d38e3713743b590917d993fb8
Daniel12346 <danezoki2@gmail.com>
2025-06-19 23:16:35 +0200

    fix: add padding to album component for better layout

--- app/(main)/albums/[id]/album.tsx
+++ app/(main)/albums/[id]/album.tsx
@@ -58,7 +58,7 @@ export default function Album() {
   }
 
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center gap-4 pb-60">
       <div className="flex flex-col gap-2">
         <AlbumCard
           id={albumWithTracks.id}
