35584d2993353fcbca0a71423abd604a3d57dbdb
Daniel12346 <danezoki2@gmail.com>
2025-09-29 23:06:18 +0200

    fix: update PlaylistsDisplay layout and correct album cover URL mapping

--- components/playlists-display.tsx
+++ components/playlists-display.tsx
@@ -8,21 +8,24 @@ interface Props {
 export default function PlaylistsDisplay({ playlists }: Props) {
   return (
     <div className="@container">
-      <div className="grid grid-cols-2 @md:grid-cols-3 @lg:grid-cols-4">
-        {playlists?.map((playlist) => (
-          <Link
-            href={`/playlists/${playlist.id}`}
-            key={playlist.id}
-            className="flex justify-center"
-          >
-            <PlaylistCard
-              {...playlist}
-              album_cover_urls={playlist.playlists_tracks.map(
-                (playlist) => playlist.albums.cover_url
-              )}
-            />
-          </Link>
-        ))}
+      <div className="grid grid-cols-2 gap-y-4 md:gap-y-0 @md:grid-cols-3 @lg:grid-cols-4">
+        {playlists?.map(
+          (playlist) =>
+            playlist && (
+              <Link
+                href={`/playlists/${playlist.id}`}
+                key={playlist.id}
+                className="flex justify-center"
+              >
+                <PlaylistCard
+                  {...playlist}
+                  album_cover_urls={playlist.playlists_tracks.map(
+                    (playlist) => playlist.track_album.cover_url
+                  )}
+                />
+              </Link>
+            )
+        )}
       </div>
     </div>
   );
