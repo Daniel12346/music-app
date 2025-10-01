f5349b2aa2f52edf930845815b5d8951bbe54a6f
Daniel12346 <danezoki2@gmail.com>
2025-10-01 20:18:23 +0200

    feat: refactor TracksLiked component to enhance track display and interaction

--- components/tracks-liked.tsx
+++ components/tracks-liked.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { getTracksLikedByUser } from "@/lib/database";
+import { addNewQueueIdToTrack } from "@/lib/utils";
 import { createClient } from "@/utils/supabase/client";
+import { XIcon } from "lucide-react";
 import useSWR from "swr";
+import LikeTrack from "./like-track";
+import TrackArtists from "./track-artists";
+import TrackOptionsButton from "./track-options";
+import { useTrackStore } from "@/state/store";
 
-export default function TracksLiked() {
+export default function TracksLiked({
+  size = 16,
+  strokeColor,
+}: {
+  size?: number;
+  strokeColor?: string;
+}) {
   const supabase = createClient();
   const { data } = useSWR("me", () =>
     supabase.auth.getUser().then((res) => res.data)
@@ -17,20 +29,60 @@ export default function TracksLiked() {
   } = useSWR(myID ? ["getTracksLikedByUser", myID] : null, () =>
     getTracksLikedByUser(supabase, myID)
   );
+  const { setCurrentTrack } = useTrackStore();
 
   return (
-    <div>
-      <span className="text-lg font-semibold text-muted-foreground">
-        Recently liked
-      </span>
-      <ul className="flex flex-col gap-1.5">
-        {myLikedTracks?.map((track) => (
-          <li key={track.id} className="flex">
-            <img src={track.album.cover_url!} width={20} alt={track.title} />
-            <span>{track.title}</span>
-          </li>
-        ))}
-      </ul>
+    <div className="flex flex-col gap-2">
+      <span className="text-lg font-semibold text-muted-foreground">Liked</span>
+
+      {myLikedTracks?.map((track) => {
+        if (!track) return null;
+        const TrackWithExtra = {
+          ...track,
+          albumCoverUrl: track.album.cover_url || "",
+          albumId: track.album.id, // Assuming albumId is the same as album ID
+          artists: track.tracks_artists.map((artist) => artist.artists) || [],
+          albumName: track.album.title || "",
+        };
+
+        return (
+          <div
+            key={track.id}
+            className="flex cursor-pointer items-center gap-2  @container"
+            onClick={
+              () => setCurrentTrack(addNewQueueIdToTrack(TrackWithExtra)) // Add queueId to track
+              //TODO: set queue to something (history tracks?)
+            }
+          >
+            <img
+              src={track.album.cover_url || ""}
+              alt={track.title}
+              className="w-10 h-10 rounded"
+            />
+            <div className="flex-1 flex flex-col">
+              <span className="text-lg ">{track.title}</span>
+
+              <TrackArtists
+                artists={track.tracks_artists.map((artist) => artist.artists)}
+              />
+            </div>
+
+            <div
+              className="flex items-center gap-1.5"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <TrackOptionsButton track={TrackWithExtra} />
+              <LikeTrack
+                trackID={track.id}
+                size={size}
+                strokeColor={strokeColor}
+                trackAlbumID={track.album.id}
+              />
+              <XIcon />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
