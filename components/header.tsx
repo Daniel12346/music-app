fd2068bef23e4541ca0dc5530e3dad3aade04c0c
Daniel12346 <danezoki2@gmail.com>
2025-10-06 23:36:21 +0200

    feature: added search bar to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,16 @@
 import AuthButton from "./header-auth";
 import { ThemeSwitcher } from "./theme-switcher";
+import { Input } from "./ui/input";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export default function Header() {
   return (
-    <div className="flex w-full gap-16 items-center justify-between px-4 py-2 bg-background border-b border-border">
-      <AuthButton />
+    <div className="flex w-full gap-4 md:gap-16 items-center justify-between px-4 py-2 bg-background border-b border-border">
+      <SidebarTrigger className="md:hidden" />
+      <div className="hidden md:flex">
+        <AuthButton />
+      </div>
+      <Input placeholder="Search..." className="max-w-lg" />
       <ThemeSwitcher />
     </div>
   );
