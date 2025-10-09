f4903c12edb400e34114c48197bf5fdfcccc0348
Daniel12346 <danezoki2@gmail.com>
2025-10-09 21:54:04 +0200

    added search results (tracks, playlists, artists and albums) in dropdown

--- components/search.tsx
+++ components/search.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Input } from "./ui/input";
 import { getSearchResults } from "@/lib/database";
 import { createClient } from "@/utils/supabase/client";
 import useSWR from "swr";
+import {
+  DiscAlbumIcon,
+  ListMusicIcon,
+  MusicIcon,
+  UserRoundIcon,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function Search() {
   const supabase = createClient();
@@ -16,7 +23,6 @@ export default function Search() {
   const tracksRes = data?.tracks ?? null;
   const artistsRes = data?.artists ?? null;
   const playlistsRes = data?.playlists ?? null;
-  console.log(albumsRes);
   return (
     <div className="max-w-lg w-full relative group">
       <Input
@@ -26,47 +32,108 @@ export default function Search() {
         onChange={(e) => setQuery(e.target.value)}
       />
 
-      <div className="absolute w-full flex-col max-h-120 overflow-y-scroll left-0  z-20 bg-accent hidden group-focus-within:flex shadow-md">
-        {albumsRes && albumsRes.status === 200 && albumsRes.data && (
-          <div className="flex flex-col gap-2 p-2">
-            <div className="font-semibold">Albums</div>
-            {albumsRes.data.map((album: any) => (
-              <div key={album.id}>
-                <a href={`/albumsRes/${album.id}`}>{album.title}</a>
+      <div className="absolute w-full  flex-col max-h-120 overflow-y-scroll left-0  z-20 bg-accent hidden group-focus-within:flex shadow-md">
+        {albumsRes &&
+          albumsRes.status === 200 &&
+          albumsRes.data?.length !== 0 && (
+            <div className="flex flex-col gap-1 p-2">
+              <div className=" flex">
+                <DiscAlbumIcon className="mr-1" />
+                Albums
               </div>
-            ))}
-          </div>
-        )}
-        {tracksRes && tracksRes.status === 200 && tracksRes.data && (
-          <div className="flex flex-col gap-2 p-2">
-            <div className="font-semibold">Tracks</div>
-            {tracksRes.data.map((track: any) => (
-              <div key={track.id}>
-                <a href={`/tracks/${track.id}`}>{track.title}</a>
+              {albumsRes?.data?.map((album) => (
+                <div key={album.id}>
+                  <Link href={`/albums/${album.id}`}>
+                    <div className="flex gap-1">
+                      <img
+                        className="rounded-sm"
+                        src={album.cover_url || ""}
+                        alt={album.title}
+                        width={40}
+                        height={40}
+                      />
+                      <div className="flex flex-col ">
+                        <span>{album.title}</span>
+                        <span className="text-s">
+                          {album.artists
+                            ?.map((artist) => artist.name)
+                            .join(", ")}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        {/* TODO: play track on click or open album with track highlighted */}
+        {tracksRes &&
+          tracksRes.status === 200 &&
+          tracksRes.data?.length !== 0 && (
+            <div className="flex flex-col gap-1 p-2">
+              <div className=" flex">
+                <MusicIcon className="mr-1" />
+                Tracks
               </div>
-            ))}
-          </div>
-        )}
-        {artistsRes && artistsRes.status === 200 && artistsRes.data && (
-          <div className="flex flex-col gap-2 p-2">
-            <div className="font-semibold">Artists</div>
-            {artistsRes.data.map((artist: any) => (
-              <div key={artist.id}>
-                <a href={`/artists/${artist.id}`}>{artist.name}</a>
+              <div className="flex flex-col gap-2 md:flex-row md:gap-4 md:overflow-x-scroll">
+                {tracksRes?.data?.map((track) => (
+                  <div key={track.id}>
+                    <Link href={`/track/${track.id}`}>
+                      <div className="flex gap-1">
+                        <img
+                          className="rounded-sm"
+                          src={track.albums_tracks[0].albums.cover_url || ""}
+                          alt={track.albums_tracks[0].albums.title}
+                          width={40}
+                          height={40}
+                        />
+                        <div className="flex flex-col ">
+                          <span>{track.title}</span>
+                          <span className="text-s">
+                            {track.tracks_artists
+                              ?.map(({ artists }) => artists.name)
+                              .join(", ")}
+                          </span>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        )}
-        {playlistsRes && playlistsRes.status === 200 && playlistsRes.data && (
-          <div className="flex flex-col gap-2 p-2">
-            <div className="font-semibold">Playlists</div>
-            {playlistsRes.data.map((playlist: any) => (
-              <div key={playlist.id}>
-                <a href={`/playlists/${playlist.id}`}>{playlist.title}</a>
+            </div>
+          )}
+        {artistsRes &&
+          artistsRes.status === 200 &&
+          artistsRes.data?.length !== 0 && (
+            <div className="flex flex-col gap-1 p-2">
+              <div className=" flex">
+                <UserRoundIcon className="mr-1" />
+                Artists
               </div>
-            ))}
-          </div>
-        )}
+              {artistsRes?.data?.map((artist) => (
+                <div key={artist.id}>
+                  <Link href={`/artists/${artist.id}`}>{artist.name}</Link>
+                </div>
+              ))}
+            </div>
+          )}
+        {playlistsRes &&
+          playlistsRes.status === 200 &&
+          playlistsRes.data?.length !== 0 && (
+            <div className="flex flex-col gap-1 p-2">
+              <div className=" flex">
+                <ListMusicIcon className="mr-1" />
+                Playlists
+              </div>
+              {playlistsRes?.data?.map((playlist) => (
+                <div key={playlist.id}>
+                  <Link href={`/playlists/${playlist.id}`}>
+                    {playlist.name}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
       </div>
     </div>
   );
