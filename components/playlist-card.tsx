9855f18d99e118de97a33174a533eb618fdf5622
Daniel12346 <danezoki2@gmail.com>
2025-10-10 21:56:01 +0200

    extracted playlists cover to a new component

--- components/playlist-card.tsx
+++ components/playlist-card.tsx
@@ -1,6 +1,7 @@
 import { Tables } from "@/database.types";
 import { cn } from "@/lib/utils";
 import LikePlaylist from "./like-playlist";
+import PlaylistCover from "./playlist-cover";
 
 type Props = Tables<"playlists"> & {
   size?: "large";
@@ -19,41 +20,12 @@ export default function PlaylistCard({
   return (
     <div className={cn("w-32 min-h-32 group", size === "large" && "w-72")}>
       <div className="relative">
-        {image_url ? (
-          <img
-            src={image_url}
-            alt={name || ""}
-            width={120}
-            height={120}
-            className={cn(
-              "rounded-md w-full object-cover h-32",
-              size === "large" && "h-72"
-            )}
-          />
-        ) : (
-          <div className="grid grid-cols-2 grid-rows-2  bg-muted border-1 border-muted-foreground rounded-md w-full">
-            {album_cover_urls
-              ?.slice(0, 4)
-              .map(
-                (album_cover_url, i) =>
-                  album_cover_url && (
-                    <img
-                      key={album_cover_url + i}
-                      src={album_cover_url}
-                      width={10}
-                      height={10}
-                      className={cn(
-                        "w-full",
-                        i === 0 && "rounded-tl-md",
-                        i === 1 && "rounded-tr-md",
-                        i === 2 && "rounded-bl-md",
-                        i === 3 && "rounded-br-md"
-                      )}
-                    />
-                  )
-              )}
-          </div>
-        )}
+        <PlaylistCover
+          alt={name}
+          image_url={image_url ?? undefined}
+          album_cover_urls={album_cover_urls}
+          size={size}
+        />
         <div className="hidden group-hover:block absolute top-0 right-0">
           <LikePlaylist playlistID={id} size={size === "large" ? 32 : 16} />
         </div>
---
+++ components/playlist-cover.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/lib/utils";
+
+interface Props {
+  alt: string;
+  image_url?: string;
+  album_cover_urls?: (string | null)[];
+  size?: "small" | "large";
+}
+export default function PlaylistCover({
+  alt,
+  image_url,
+  album_cover_urls,
+  size,
+}: Props) {
+  return image_url ? (
+    <img
+      src={image_url}
+      alt={alt}
+      width={120}
+      height={120}
+      className={cn("rounded-md w-full object-cover h-32", {
+        "h-20 w-20": size === "small",
+        "h-72 w-72": size === "large",
+      })}
+    />
+  ) : (
+    <div
+      className={cn(
+        "grid grid-cols-2 grid-rows-2  bg-muted border-1 border-muted-foreground rounded-md",
+        { "h-20 w-20": size === "small", "h-72 w-72": size === "large" }
+      )}
+    >
+      {album_cover_urls
+        ?.slice(0, 4)
+        .map(
+          (album_cover_url, i) =>
+            album_cover_url && (
+              <img
+                key={album_cover_url + i}
+                src={album_cover_url}
+                width={10}
+                height={10}
+                className={cn(
+                  "w-full",
+                  i === 0 && "rounded-tl-md",
+                  i === 1 && "rounded-tr-md",
+                  i === 2 && "rounded-bl-md",
+                  i === 3 && "rounded-br-md"
+                )}
+              />
+            )
+        )}
+    </div>
+  );
+}
