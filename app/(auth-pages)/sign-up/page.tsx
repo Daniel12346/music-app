ead41fec30a1787cad5e4ef282a5cae1ee48da30
Daniel12346 <danezoki2@gmail.com>
2025-10-17 22:49:24 +0200

    changed background to match sign-in form

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -20,8 +20,8 @@ export default async function Signup(props: {
   return (
     <div className="w-full flex flex-col items-center p-2 md: pt-12">
       <form
-        className="flex-1 flex flex-col w-full max-w-80 bg-white p-3 md:p-6 rounded-md
-      border-2 border-foreground"
+        className="flex-1 flex flex-col w-full max-w-80 bg-slate-200/50 p-3 md:p-6 rounded-md
+      border-2 border-muted-foreground"
       >
         <h1 className="text-2xl font-medium">Sign up</h1>
         <p className="text-sm text text-foreground">
@@ -30,7 +30,7 @@ export default async function Signup(props: {
             Sign in
           </Link>
         </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+        <div className="flex flex-col gap-2 [&>input]:mb-3 [&>input]:bg-white mt-8">
           <Label htmlFor="email">Email</Label>
           <Input name="email" placeholder="you@example.com" required />
           <Label htmlFor="password">Password</Label>
