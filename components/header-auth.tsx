4cd60be1b2f6a6b4971d2d688ab72e3109a8da31
Daniel12346 <danezoki2@gmail.com>
2025-10-24 23:58:40 +0200

    added user avatar icon with dropdown

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -2,22 +2,48 @@ import { signOutAction } from "@/app/actions";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { Avatar } from "@radix-ui/react-avatar";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { AvatarImage } from "./ui/avatar";
+import { getProfile } from "@/lib/database";
 
 export default async function AuthButton() {
   const supabase = await createClient();
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  const userProfile = await getProfile(supabase, user?.id!);
 
   return user ? (
     <div className="flex items-center gap-4 w-full">
       {/* TODO: user profile image */}
-      <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
-          Sign out
-        </Button>
-      </form>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="w-10 h-10">
+            <AvatarImage
+              className="rounded-full "
+              src={userProfile?.avatar_url || ""}
+              alt={userProfile?.username || ""}
+            />
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <form action={signOutAction}>
+              <Button type="submit" variant={"outline"}>
+                Sign out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   ) : (
     <div className="flex gap-2">
