22c79a7166edb5482acf3aeacfa2276a282da223
Daniel12346 <danezoki2@gmail.com>
2025-10-28 23:21:44 +0100

    added user avatar with a dropdown

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,7 +1,7 @@
+"use client";
 import { signOutAction } from "@/app/actions";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { createClient } from "@/utils/supabase/server";
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { Avatar } from "@radix-ui/react-avatar";
 import {
@@ -10,34 +10,55 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { AvatarImage } from "./ui/avatar";
+import { AvatarFallback, AvatarImage } from "./ui/avatar";
 import { getProfile } from "@/lib/database";
+import useSWR from "swr";
+import { createClient } from "@/utils/supabase/client";
+import { useRef } from "react";
 
-export default async function AuthButton() {
-  const supabase = await createClient();
+export default function AuthButton() {
+  const supabase = createClient();
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const userProfile = await getProfile(supabase, user?.id!);
-
-  return user ? (
+    data: myData,
+    isLoading: isMyDataLoading,
+    isValidating: isMyDataValidating,
+  } = useSWR("me", () => supabase.auth.getUser().then((res) => res.data));
+  const {
+    data: myProfile,
+    isLoading,
+    isValidating,
+  } = useSWR(myData?.user?.id ? ["getProfile", myData?.user?.id] : null, () =>
+    getProfile(supabase, myData?.user?.id!)
+  );
+  const inputRef = useRef<HTMLInputElement>(null);
+  return myData ? (
     <div className="flex items-center gap-4 w-full">
       {/* TODO: user profile image */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Avatar className="w-10 h-10">
+          <Avatar className="w-10 h-10 group">
             <AvatarImage
-              className="rounded-full "
-              src={userProfile?.avatar_url || ""}
-              alt={userProfile?.username || ""}
+              className="rounded-full cursor-pointer"
+              src={myProfile?.avatar_url || ""}
+              alt={myProfile?.username || ""}
             />
+            <AvatarFallback>{myProfile?.username?.charAt(0)}</AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuContent className="w-fit">
+          <DropdownMenuLabel className="text-md">
+            {myProfile?.username}
+          </DropdownMenuLabel>
+          <div className="px-3 cursor-pointer">
+            <span>Account settings</span>
+          </div>
           <DropdownMenuItem>
             <form action={signOutAction}>
-              <Button type="submit" variant={"outline"}>
+              <Button
+                type="submit"
+                variant={"secondary"}
+                className="cursor-pointer"
+              >
                 Sign out
               </Button>
             </form>
@@ -46,13 +67,15 @@ export default async function AuthButton() {
       </DropdownMenu>
     </div>
   ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
-    </div>
+    !isLoading && !isValidating && !isMyDataLoading && !isMyDataValidating && (
+      <div className="flex gap-2">
+        <Button asChild size="sm" variant={"outline"}>
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
+        <Button asChild size="sm" variant={"default"}>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+    )
   );
 }
