263681be2db623e1e9abccb4f88ae50334f410f8
Daniel12346 <danezoki2@gmail.com>
2025-10-28 22:57:11 +0100

    moved user avatar to the right

--- components/header.tsx
+++ components/header.tsx
@@ -7,12 +7,11 @@ export default function Header() {
   return (
     <div className="flex w-full gap-4 md:gap-16 items-center justify-between px-4 py-2 bg-background border-b border-border">
       <SidebarTrigger className="md:hidden" />
-      <div className="hidden md:flex">
-        <AuthButton />
-      </div>
+      <div className="w-20 hidden md:flex"></div>
       <Search />
-      <div className="w-6 flex justify-center">
+      <div className="flex w-20 gap-3 justify-between items-center">
         <ThemeSwitcher />
+        <AuthButton />
       </div>
     </div>
   );
