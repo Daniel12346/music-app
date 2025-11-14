6415504f99c264dc263238715bead8a754ec6773
Daniel12346 <danezoki2@gmail.com>
2025-11-14 20:41:40 +0100

    refactor: used former /me page as home page

---
+++ app/(main)/me.tsx
@@ -0,0 +1,52 @@
+"use client";
+import AlbumsDisplay from "@/components/albums-display";
+import MyPlaylists from "@/components/my-playlists";
+import { getAlbumsLikedByUser } from "@/lib/database";
+import { createClient } from "@/utils/supabase/client";
+import useSWR from "swr";
+
+export default function Me() {
+  const supabase = createClient();
+  const { data, isLoading: isMyDataLoading } = useSWR("me", () =>
+    supabase.auth.getUser().then((res) => res.data)
+  );
+  const myID = data?.user?.id;
+
+  const {
+    data: myLikedAlbums,
+    error: albumsError,
+    isLoading: areAlbumsLoading,
+  } = useSWR(myID ? ["getAlbumsLikedByUser", myID] : null, () =>
+    getAlbumsLikedByUser(supabase, myID)
+  );
+  if (albumsError) {
+    return <div>Error loading albums</div>;
+  }
+  if (!areAlbumsLoading && myLikedAlbums?.length === 0) {
+    return <div className="bg-red-400">No albums found</div>;
+  }
+  return (
+    <>
+      {myID && (
+        <div className="px-3">
+          <h1 className="mb-2 text-lg">Playlists</h1>
+          <div className="mb-4">
+            <MyPlaylists />
+          </div>
+          <h1 className="mb-2 text-lg">Liked albums</h1>
+          <div className="">
+            <AlbumsDisplay
+              albums={myLikedAlbums ?? null}
+              isLoading={isMyDataLoading || areAlbumsLoading}
+            />
+          </div>
+        </div>
+      )}
+      {!myID && !isMyDataLoading && (
+        <div>
+          
+        </div>
+      )}
+    </>
+  );
+}
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,23 @@
+import { createClient } from "@/utils/supabase/server";
+import { SWRConfig, unstable_serialize } from "swr";
+import Me from "./me";
+import { getAlbumsLikedByUser } from "@/lib/database";
+
 export default async function Home() {
+  const supabase = await createClient();
+  const { data: meData } = await supabase.auth.getUser();
+  const myID = meData.user?.id;
+  const myLikedAlbums = await getAlbumsLikedByUser(supabase, myID);
+
   return (
-    <>
-      <main className="flex-1 flex flex-col gap-6 px-4">Music app</main>
-    </>
+    <SWRConfig
+      value={{
+        fallback: {
+          [unstable_serialize(["getAlbumsLikedByUser", myID])]: myLikedAlbums,
+        },
+      }}
+    >
+      <Me />
+    </SWRConfig>
   );
 }
