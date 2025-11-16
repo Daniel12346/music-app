7a52c6a3ce9821bd4c02428ff52f97dd1a3ab3f3
Daniel12346 <danezoki2@gmail.com>
2025-11-16 21:13:09 +0100

    added a usename input field, removed placeholders

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -32,15 +32,11 @@ export default async function Signup(props: {
         </p>
         <div className="flex flex-col gap-2 [&>input]:mb-3 [&>input]:bg-white mt-8">
           <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
+          <Input name="email" required />
+          <Label htmlFor="username">Username</Label>
+          <Input name="username" required />
           <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
+          <Input type="password" name="password" minLength={6} required />
           <SubmitButton formAction={signUpAction} pendingText="Signing up...">
             Sign up
           </SubmitButton>
