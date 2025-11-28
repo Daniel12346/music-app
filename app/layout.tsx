fc8281294abae3211cc666800b3e209953ce3f40
Daniel12346 <danezoki2@gmail.com>
2025-11-28 19:39:41 +0100

    remove unused imports and metadata from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
-import AudioPlayer from "@/components/audio-player";
-import SidebarMobile from "@/components/sidebar-mobile";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
 
 const geistSans = Geist({
   display: "swap",
