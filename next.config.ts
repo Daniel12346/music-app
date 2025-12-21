772675a55514ed3346367689808335a4e30efda9
Daniel12346 <danezoki2@gmail.com>
2025-12-21 20:07:42 +0100

    added allowed config for remote images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        port: "",
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL!.replace("https://", ""),
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
