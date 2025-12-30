3be93cc9be8dfddacf9536f2948868936567695b
Daniel12346 <danezoki2@gmail.com>
2025-12-30 22:38:56 +0100

    moved liked albums to a separate component

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,46 +1,23 @@
 "use client";
 import AlbumsDisplay from "@/components/albums-display";
+import LikedAlbums from "@/components/liked-albums";
 import MyPlaylists from "@/components/my-playlists";
 import { getAlbumsLikedByUser } from "@/lib/database";
 import { createClient } from "@/utils/supabase/client";
 import useSWR from "swr";
 
 export default function Home() {
-  const supabase = createClient();
-  const { data, isLoading: isMyDataLoading } = useSWR("me", () =>
-    supabase.auth.getUser().then((res) => res.data)
-  );
-  const myID = data?.user?.id;
-
-  const {
-    data: myLikedAlbums,
-    error: albumsError,
-    isLoading: areAlbumsLoading,
-  } = useSWR(myID ? ["getAlbumsLikedByUser", myID] : null, () =>
-    getAlbumsLikedByUser(supabase, myID)
-  );
-  if (albumsError) {
-    return <div>Error loading albums</div>;
-  }
   return (
-    myID && (
-      <div className="px-3">
-        <h1 className="mb-2 text-xl font-bold text-foreground/90">
-          Your playlists
-        </h1>
-        <div className="mb-4">
-          <MyPlaylists />
-        </div>
-        <h1 className="mb-2 text-xl font-bold text-foreground/90">
-          Liked albums
-        </h1>
-        <div className="">
-          <AlbumsDisplay
-            albums={myLikedAlbums ?? null}
-            isLoading={isMyDataLoading || areAlbumsLoading}
-          />
-        </div>
+    <div className="px-3">
+      <h2 className="mb-2 text-xl font-bold text-foreground/90">
+        Your playlists
+      </h2>
+      <div className="mb-4">
+        <MyPlaylists />
       </div>
-    )
+      <div>
+        <LikedAlbums />
+      </div>
+    </div>
   );
 }
