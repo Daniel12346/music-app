6dadfde7d0abeb207e1f01d82f4a847f2fccf830
Daniel12346 <danezoki2@gmail.com>
2025-12-30 22:46:54 +0100

    moved heading for MyPlaylists inside that component, removed unused imports

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,17 +1,10 @@
 "use client";
-import AlbumsDisplay from "@/components/albums-display";
 import LikedAlbums from "@/components/liked-albums";
 import MyPlaylists from "@/components/my-playlists";
-import { getAlbumsLikedByUser } from "@/lib/database";
-import { createClient } from "@/utils/supabase/client";
-import useSWR from "swr";
 
 export default function Home() {
   return (
     <div className="px-3">
-      <h2 className="mb-2 text-xl font-bold text-foreground/90">
-        Your playlists
-      </h2>
       <div className="mb-4">
         <MyPlaylists />
       </div>
