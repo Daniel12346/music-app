7ebc6e8d867a1ce2ded4f87c21489ab3bed33a80
Daniel12346 <danezoki2@gmail.com>
2026-01-08 23:25:12 +0100

    updated UserAvatar component to use server-side supabase client

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { signOutAction } from "@/app/actions";
 import Link from "next/link";
 import { Button } from "./ui/button";
@@ -13,21 +12,19 @@ import {
 import { AvatarFallback, AvatarImage } from "./ui/avatar";
 import { getUserProfile } from "@/lib/database";
 import useSWR from "swr";
-import { createClient } from "@/utils/supabase/client";
 import { UserRoundCogIcon } from "lucide-react";
+import { createClient } from "@/utils/supabase/server";
 
-export default function UserAvatar() {
-  const supabase = createClient();
-  const { data: myData, isLoading: isMyDataLoading } = useSWR("me", () =>
-    supabase.auth.getUser().then((res) => res.data)
-  );
+export default async function UserAvatar() {
+  const supabase = await createClient();
+  const { data: myData } = await supabase.auth.getUser();
   const { data: myProfile } = useSWR(
     myData?.user?.id ? ["getUserProfile", myData?.user?.id] : null,
     () => getUserProfile(supabase, myData?.user?.id!)
   );
   return (
     <div className="flex items-center gap-4 w-full justify-end">
-      {myData?.user?.id && !isMyDataLoading && (
+      {myData?.user?.id && (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Avatar className="w-10 h-10 group">
