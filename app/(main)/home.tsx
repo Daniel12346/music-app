ab1744303126388ae1b14646467b9f4e4270b41c
Daniel12346 <danezoki2@gmail.com>
2026-01-09 23:57:13 +0100

    moved headings inside the appropriate components so their rendering can be skipped if user is not logged in

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -7,45 +7,12 @@ export default function Home() {
   return (
     <div className="px-3 flex flex-col gap-8">
       <div>
-        <div className="flex justify-between pr-6">
-          <h2 className="mb-2 text-xl font-bold text-foreground/90">
-            New albums
-          </h2>
-          <Link
-            href="/albums/new"
-            className="hover:underline flex items-end pb-1 text-muted-foreground"
-          >
-            See all
-          </Link>
-        </div>
         <NewAlbumsLimited />
       </div>
       <div>
-        <div className="flex justify-between pr-6">
-          <h2 className="mb-2 text-xl font-bold text-foreground/90">
-            Your playlists
-          </h2>
-          <Link
-            href="/playlists/me"
-            className="hover:underline flex items-end pb-1 text-muted-foreground"
-          >
-            See all
-          </Link>
-        </div>
         <MyPlaylistsLimited />
       </div>
       <div>
-        <div className="flex justify-between pr-6">
-          <h2 className="mb-2 text-xl font-bold text-foreground/90">
-            Liked albums
-          </h2>
-          <Link
-            href="/albums/liked"
-            className="hover:underline flex items-end pb-1 text-muted-foreground"
-          >
-            See all
-          </Link>
-        </div>
         <LikedAlbumsLimited />
       </div>
     </div>
