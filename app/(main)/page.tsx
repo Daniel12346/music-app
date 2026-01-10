3ae455d7594a0bfb7946b9fa2cb5d6982d5344b5
Daniel12346 <danezoki2@gmail.com>
2026-01-11 00:01:22 +0100

    fix: add prefetching for new tracks by liked artists in SWRConfig

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,6 +3,7 @@ import { SWRConfig, unstable_serialize } from "swr";
 import {
   getAlbumsLikedByUser,
   getNewAlbums,
+  getNewTracksByLikedArtists,
   getUserPlaylistsWithPreview,
 } from "@/lib/database";
 import Home from "./home";
@@ -14,6 +15,8 @@ export default async function HomePage() {
   return (
     <SWRConfig
       value={{
+        //prefetching data if user is logged in
+        //all the promises passed to fetch fallback data resolve in parallel
         fallback: myID
           ? {
               ["me"]: myData,
@@ -25,9 +28,12 @@ export default async function HomePage() {
                 supabase,
                 4
               ),
+              //getNewTracksByLikedArtists is used in the new tracks playlist in my playlists on the home page
+              [unstable_serialize(["getNewTracksByLikedArtists", myID])]:
+                getNewTracksByLikedArtists(supabase, myID!),
             }
-            //getNewAlbums is prefetched even if user is not logged in because it doesn't use myID
-          : {
+          : //getNewAlbums is prefetched even if user is not logged in because it doesn't use myID
+            {
               [unstable_serialize(["getNewAlbums", 4])]: getNewAlbums(
                 supabase,
                 4
