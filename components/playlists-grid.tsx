8ea7602d25f24c6cd7fabf522faf7394c3462b21
Daniel12346 <danezoki2@gmail.com>
2026-01-11 00:01:46 +0100

    fix: remove loading state and skeleton placeholders from PlaylistsGrid component

--- components/playlists-grid.tsx
+++ components/playlists-grid.tsx
@@ -1,6 +1,5 @@
 import PlaylistCard from "./playlist-card";
 import { PlaylistsWithPreview } from "@/lib/database";
-import { Skeleton } from "./ui/skeleton";
 import CreatePlaylist from "./create-playlist";
 interface Props {
   playlists: PlaylistsWithPreview;
@@ -8,43 +7,24 @@ interface Props {
   withCreateNew?: boolean;
 }
 
-export default function PlaylistsGrid({
-  playlists,
-  isLoading = true,
-  withCreateNew,
-}: Props) {
+export default function PlaylistsGrid({ playlists, withCreateNew }: Props) {
   return (
     <div className="@container">
       <ul className="grid grid-cols-2 justify-items-center @md:grid-cols-3 @lg:grid-cols-4 gap-y-6">
         {withCreateNew && <CreatePlaylist />}
-        {isLoading ? (
-          Array.from({ length: 4 }).map((_, i) => (
-            <li
-              className="flex justify-center opacity-90 dark:opacity-40"
-              key={i}
-            >
-              <div className="w-32">
-                <Skeleton className="w-full h-32 bg-slate-500" />
-                <Skeleton className="w-full mt-1 h-6 bg-slate-500" />
-              </div>
-            </li>
-          ))
-        ) : (
-          <>
-            {playlists?.map(
-              (playlist) =>
-                playlist && (
-                  <PlaylistCard
-                    {...playlist}
-                    key={playlist.id}
-                    album_cover_urls={playlist.playlists_tracks.map(
-                      (playlist) => playlist.track_album.cover_url
-                    )}
-                  />
-                )
-            )}
-          </>
+        {playlists?.map(
+          (playlist) =>
+            playlist && (
+              <PlaylistCard
+                {...playlist}
+                key={playlist.id}
+                album_cover_urls={playlist.playlists_tracks.map(
+                  (playlist) => playlist.track_album.cover_url
+                )}
+              />
+            )
         )}
+       
       </ul>
     </div>
   );
