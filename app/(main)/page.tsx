00f3a04d8703cca7072f1c3f4ff1e846dd852fc4
Daniel12346 <danezoki2@gmail.com>
2026-01-10 23:32:13 +0100

    fix: refactor fallback logic in SWRConfig to include user data and limit parameters for fetching liked albums and user playlists

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,20 +1,38 @@
 import { createClient } from "@/utils/supabase/server";
 import { SWRConfig, unstable_serialize } from "swr";
-import { getAlbumsLikedByUser } from "@/lib/database";
+import {
+  getAlbumsLikedByUser,
+  getNewAlbums,
+  getUserPlaylistsWithPreview,
+} from "@/lib/database";
 import Home from "./home";
 
 export default async function HomePage() {
   const supabase = await createClient();
-  const { data: meData } = await supabase.auth.getUser();
-  const myID = meData.user?.id;
-  const myLikedAlbums = await getAlbumsLikedByUser(supabase, myID);
-
+  const { data: myData } = await supabase.auth.getUser();
+  const myID = myData.user?.id;
   return (
     <SWRConfig
       value={{
-        fallback: {
-          [unstable_serialize(["getAlbumsLikedByUser", myID])]: myLikedAlbums,
-        },
+        fallback: myID
+          ? {
+              ["me"]: myData,
+              [unstable_serialize(["getAlbumsLikedByUser", myID, 4])]:
+                getAlbumsLikedByUser(supabase, myID!, 4),
+              [unstable_serialize(["getUserPlaylistsWithPreview", myID, 4])]:
+                getUserPlaylistsWithPreview(supabase, myID!, 4),
+              [unstable_serialize(["getNewAlbums", 4])]: getNewAlbums(
+                supabase,
+                4
+              ),
+            }
+            //getNewAlbums is prefetched even if user is not logged in because it doesn't use myID
+          : {
+              [unstable_serialize(["getNewAlbums", 4])]: getNewAlbums(
+                supabase,
+                4
+              ),
+            },
       }}
     >
       <Home />
